18409648f606f557f7a0ef848bc831037d5a6611
Kauan <162659253+solidsoon@users.noreply.github.com>
2024-05-20 23:42:40 -0300

    Sistem Deploy

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Calculadora from './components/Calculadora/Calculadora'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <App/>
+    </div>
+  )
+}
